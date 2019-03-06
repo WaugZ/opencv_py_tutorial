58557e63ec246a442a1c8dabc8e28a77ae6a0eaa
waugz <waugz@icloud.com>
2019-03-06 19:18:36 +0800

    improve

--- label.py
+++ label.py
@@ -1,8 +1,7 @@
 import cv2
 
-
-pt_start = (0, 0)
-pt_end = (0, 0)
+pt_start = (-1, -1)
+pt_end = (-1, -1)
 label_counter = 0
 
 
@@ -16,16 +15,19 @@ def on_mouse(event, x, y, flags, param):
             label_counter += 1
 
 
-if __name__ == "__main__":
-    img = cv2.imread("test.jpg")
+def labeling(filename):
+    global label_counter
+    img = cv2.imread(filename)
     copy_img = img.copy()
     h, w = img.shape[:2]
     cv2.namedWindow("test", cv2.WINDOW_NORMAL)
     cv2.setMouseCallback("test", on_mouse, 0)
     prev_count = label_counter
+    labels = []
     while True:
         cv2.imshow("test", img)
         if cv2.waitKey(10) == ord('q'):
+            label_counter = 0
             break
         if prev_count != label_counter:
             cv2.rectangle(img, pt_start, pt_end, (0, 0, 255), int(h / 150))
@@ -40,4 +42,10 @@ if __name__ == "__main__":
                 label_pt[1] -= height
                 height = -height
             print(label_pt, width, height)
+            labels.append([tuple(label_pt), width, height])
+    print(labels)
+
+
+if __name__ == "__main__":
+    labeling("test2.png")
 
